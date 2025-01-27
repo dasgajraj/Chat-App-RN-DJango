601d5a1b57fe2f38f653e9b0912a447444b0c8cd
dasgajraj <dasgajrajg@gmail.com>
2025-01-28 00:06:12 +0530

    better console statements for the readability and the merger the first name and last name field into one,a dded tokens, Fixed the Django server run and added the api calling through axios

---
+++ Frontend/core/utils.js
@@ -0,0 +1,13 @@
+function log() {
+  // a custom log statements that indents
+  // object for better readability
+  for (let i = 0; i < arguments.length; i++) {
+    var arg = arguments[i];
+    // Stringigy and indent Object
+    if (typeof arg == "object") {
+      arg = JSON.stringify(arg, null, 2);
+    }
+    console.log(arg);
+  }
+}
+export default { log };
